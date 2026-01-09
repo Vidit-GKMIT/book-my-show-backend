bf147c9bb1490d5269236503cf0f7be46a233991
Vidit Khandelwal <vidit@gkmit.co>
2026-01-09 13:07:01 +0530

    feat: add controllers, services for auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,78 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Headers,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { loginDTO, registerDTO } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerAuthDto: registerDTO) {
+    await this.authService.register(registerAuthDto);
+    return {
+      status: 200,
+      message: 'User registered successfully',
+    };
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('refresh')
+  async refresh(@Headers('authorization') authHeader: string) {
+    const accessToken = await this.authService.refresh(authHeader);
+    return {
+      accessToken,
+    };
+  }
+
+  @Post('login')
+  async login(@Body() loginDTO: loginDTO) {
+    await this.authService.login(loginDTO);
+    return {
+      status: 200,
+      message: 'OTP send successfully',
+    };
+  }
+
+  @Post('verify-otp')
+  async verifyOtp(@Body() data: { email: string; otp: number }) {
+    const tokens = await this.authService.verifyOtp(data);
+    return {
+      message: 'User logged in successfully',
+      status: 200,
+      accessToken: tokens.accessToken,
+      refreshToken: tokens.refreshToken,
+    };
+  }
+
+  @Get()
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.authService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+    return this.authService.update(+id, updateAuthDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.authService.remove(+id);
+  }
+}
